f5caa241810e7e639f239ee759f6f3f12e4ce641
Eldar4432 <aibekov4432@gmail.com>
2025-11-20 00:58:20 +0600

    created ChannelList component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import { fetchUsers } from "./api/users";
+import ChannelList from "./components/ChannelList/ChannelList";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [currentChannel, setCurrentChannel] = useState(null);
+
+  // пока что заглушки
+  const channels = [
+    { channelId: "1", name: "Общий", memberCount: 3 },
+    { channelId: "2", name: "Frontend", memberCount: 2 },
+  ];
 
   useEffect(() => {
     fetchUsers().then((data) => setUsers(data));
@@ -11,6 +19,11 @@ function App() {
   return (
     <div style={{ padding: 20 }}>
       <h1>Real-Time Chat</h1>
+      <ChannelList
+        channels={channels}
+        currentChannel={currentChannel}
+        onJoin={(id) => setCurrentChannel(id)}
+      />
 
       <h2>Пользователи:</h2>
       <ul>
